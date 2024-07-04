38ac99287bfaa6a9558d2867260f27256d671f47
J445dx <jheysonpereira439@gmail.com>
2024-07-03 22:09:29 -0300

    Delete high_jump.lua

--- high_jump.lua
+++
@@ -1,7 +0,0 @@
-function AdjustJump(value)
-    local player = game.Players.LocalPlayer
-    local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
-    if humanoid then
-        humanoid.JumpPower = value
-    end
-end
