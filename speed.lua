2bd55cfd905a4c7f95133b9ff55e34d8a5be6d01
J445dx <jheysonpereira439@gmail.com>
2024-07-03 21:43:52 -0300

    Delete speed.lua

--- speed.lua
+++
@@ -1,7 +0,0 @@
-function AdjustSpeed(value)
-    local player = game.Players.LocalPlayer
-    local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
-    if humanoid then
-        humanoid.WalkSpeed = value
-    end
-end
